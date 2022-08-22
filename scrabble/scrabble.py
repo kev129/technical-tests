a81900345aa0d5f09a483fe157d0941aad12d01a
kev129 <kevin-nguyen129@hotmail.com>
2022-08-22 11:33:37 +0100

    Starting task: Planning functions

---
+++ scrabble/scrabble.py
@@ -0,0 +1,17 @@
+# Functions required:
+# Get score, Get tiles, Randomize bag, Find valid word, Find longest valid word, Find highest scoring word, Find highest scoring word if one of the letters can score triple
+
+# Test case: GUARDIAN = 2 + 1 + 1 + 1 + 2 + 1 + 1 + 1 = 10
+
+
+# Declare points of each letter
+# Declare distribution of letters in bag
+
+
+# Function to randomize bag
+
+# Get tiles
+
+# Find Valid word
+
+# Get score
